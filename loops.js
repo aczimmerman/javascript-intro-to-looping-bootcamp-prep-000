0859f91e6358be817f213a392705b697fd9ed130
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 19:11:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop (array) {
-  var array = makeArray()
+  var array = []
   for (var i = 0; i < 25; i++) {
     if i === 1 {
       array.push("I am 1 strange loop.")
@@ -7,4 +7,5 @@ function forLoop (array) {
       array.push("I am ${i} strange loop.")
     }
   }
+  return array
 }
